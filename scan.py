d1d72a6b3a8c146b82f05c3e5c7e845eceb01a78
ravialuthge <aluthgeravi@gmail.com>
2021-04-26 10:49:52 +0530

    update

--- scan.py
+++ scan.py
@@ -2,7 +2,7 @@ import os
 install_version = os.system('docker version --format "{{.Server.Version}}"')
 latest_version_cmd = os.system('yum list docker-ce --showduplicates | sort -r | awk "{print $2}" | sed -n 6p')
 latest_version_str = str(latest_version_cmd)
-latest_version_str_x = latest_version_str.split() 
+latest_version_str_x = latest_version_str.split(':','-') 
 latest_version = latest_version_str_x[1]
 print(latest_version)
 print(latest_version_str_x)
