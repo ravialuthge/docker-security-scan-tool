f39b038b86715daca98116b0d6f0ebb572af295e
ravialuthge <aluthgeravi@gmail.com>
2021-04-26 11:04:46 +0530

    update

--- scan.py
+++ scan.py
@@ -4,11 +4,11 @@ latest_version_cmd = os.system('yum list docker-ce --showduplicates | sort -r | 
 latest_version_str = str(latest_version_cmd)
 latest_version_str_x = latest_version_str.split(':-') 
 latest_version = latest_version_str_x[1]
-print(latest_version)
-print(latest_version_cmd)
-print(latest_version_str)
+#print(latest_version)
+#print(latest_version_cmd)
+#print(latest_version_str)
 print(install_version)
-print(latest_version_str_x)
+#print(latest_version_str_x)
 if install_version == latest_version:
 	print ("Docker is up to date")
 else:
