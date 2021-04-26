2948df92fd52334aaa9ab55359cfa34efad72c08
ravialuthge <aluthgeravi@gmail.com>
2021-04-26 09:57:51 +0530

    update

--- scan.py
+++ scan.py
@@ -1,7 +1,9 @@
 import os
 install_version = os.popen('docker version --format "{{.Server.Version}}"')
-current_version = '20.10.6'
-if install_version == current_version:
+latest_version_str = os.popen('yum list docker-ce  | sort -r | awk "{print $2}" | sed -n 6p')
+latest_version_str_x = latest_version_str.split("[:-]") 
+latest_version = latest_version_str_x[1]
+if install_version == latest_version:
 	print ("Docker is up to date")
 else:
 	print ("Docker not update")
