84b5e1d8dc83f20b2c0e8a1f6a80c78bba3e0f7d
ravialuthge <aluthgeravi@gmail.com>
2021-04-26 10:31:56 +0530

    update

--- scan.py
+++ scan.py
@@ -1,6 +1,6 @@
 import os
 install_version = os.popen('docker version --format "{{.Server.Version}}"')
-latest_version_str = os.popen('yum list docker-ce  | sort -r | awk "{print $2}" | sed -n 6p')
+str(latest_version_str) = os.popen('yum list docker-ce  | sort -r | awk "{print $2}" | sed -n 6p')
 latest_version_str_x = latest_version_str.split() 
 latest_version = latest_version_str_x[1]
 if install_version == latest_version:
