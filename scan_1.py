65c2c0cef18d9b65b11b77631456b576d00a4687
ravialuthge <aluthgeravi@gmail.com>
2021-04-26 22:36:53 +0530

    update

---
+++ scan_1.py
@@ -0,0 +1,16 @@
+import os
+import subprocess
+import re
+
+root_dir_ch_cmd = "df -h | grep $(docker info -f '{{ .DockerRootDir }}') | awk '{print $6}'"
+root_dir_output = subprocess.check_output(["docker", "info" , "--format" , "'{{.DockerRootDir}}'"])
+root_dir_x = root_dir_output.decode("utf-8")
+root_dir = root_dir_x.replace("'",'')
+root_dir_ch_output = os.popen(root_dir_ch_cmd).read()
+root_dir_ch = root_dir_ch_output.rstrip()
+print(root_dir)
+print(root_dir_ch)
+if root_dir == root_dir_ch:
+	print ("crated separate partition for docker root directory")
+else:
+    print ("not crated separate partition for docker root directory")
