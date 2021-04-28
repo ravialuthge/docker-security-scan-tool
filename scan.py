5f0481d79856b84ab04a1580877af9d598ab52bd
ravialuthge <aluthgeravi@gmail.com>
2021-04-28 07:48:12 +0530

    update

--- scan.py
+++ scan.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 import re
 
-def docker_version(docker_version_re):
+def docker_version():
 	latest_version_cmd = "yum list docker-ce | sort -r | awk '{print $2}' | sed -n 6p"
 	install_version_output = subprocess.check_output(["docker", "version" , "--format" , "'{{.Server.Version}}'"])
 	install_version_x = install_version_output.decode("utf-8")
@@ -18,8 +18,9 @@ def docker_version(docker_version_re):
 		docker_version_re = "Docker not update"
 	else:
 		docker_version_re = "Docker not install"
+	return docker_version_re
 
-def docker_root(docker_root_re):
+def docker_root():
 	root_dir_ch_cmd = "df -h | grep $(docker info -f '{{ .DockerRootDir }}') | awk '{print $6}'"
 	root_dir_output = subprocess.check_output(["docker", "info" , "--format" , "'{{.DockerRootDir}}'"])
 	root_dir_x = root_dir_output.decode("utf-8")
@@ -31,6 +32,7 @@ def docker_root(docker_root_re):
 		docker_root_re = "crated separate partition for docker root directory"
 	else:
 		docker_root_re = "not crated separate partition for docker root directory"
+	return docker_root_re
 
 def output():
 	print ('# --------------------------------------------------------------------------------------------\n\
@@ -40,5 +42,6 @@ def output():
 	')
 	print ('INFO   ', docker_version_re)
 	print ('WARN   ', docker_root_re)
-     
-docker_version()
+
+if __name__ == "__main__":     
+	output()
