bc742b58edd6521d95a4c81c60cb91b86d8b8fdc
ravialuthge <aluthgeravi@gmail.com>
2021-04-28 08:19:11 +0530

    update

--- scan.py
+++ scan.py
@@ -43,6 +43,7 @@ def output():
 # # v1.0.0 - 04-22-2015\n\
 # # ------------------------------------------------------------------------------------------\n\
 	', 'green'))
+	print (colored('Docker Host',attrs=['bold']))
 	print (colored('INFO   ', 'blue'), docker_version_re)
 	print (colored('WARN   ', 'red'), docker_root_re)
 
