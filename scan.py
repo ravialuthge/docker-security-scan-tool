f4d4e7de297afc8d4544d3c8fc48a8f78c757024
ravialuthge <aluthgeravi@gmail.com>
2021-04-29 11:52:07 +0530

    update

--- scan.py
+++ scan.py
@@ -45,12 +45,12 @@ def container_user():
 	container_users = container_user_output.split()
 	f = open("re.txt", "w")
 	for i in (container_users):
-	if i == 'User=' or i == 'User=root':
+	 if i == 'User=' or i == 'User=root':
 			container_user_co = "not user for the container has been created"
-	else:
+	 else:
 			container_user_co = "user for the container has been created"
-	f.write(container_user_co)
-	f.write("\n")
+	 f.write(container_user_co)
+	 f.write("\n")
 	f= open("re.txt", "r")
 	container_user_co_f = f.read()
 	table = [[images_output , container_user_co_f]]
