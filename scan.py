c80e064b923c033d62cf3c4bbaeca9add46589a7
ravialuthge <aluthgeravi@gmail.com>
2021-04-29 12:27:13 +0530

    update

--- scan.py
+++ scan.py
@@ -44,16 +44,23 @@ def container_user():
 	images = images_output.split()
 	container_users = container_user_output.split()
 	f = open("re.txt", "w")
+	f_st = open("re_st.txt", "w")
 	for i in (container_users):
 	 if i == 'User=' or i == 'User=root':
-			container_user_co = colored('not user for the container has been created' , 'red')
+			container_user_co = 'not user for the container has been created'
+			container_user_co_st = colored('WARN', 'red')
 	 else:
-			container_user_co = colored('user for the container has been created' , 'green')
+			container_user_co = 'user for the container has been created'
+			container_user_co_st = colored('PASS', 'green')
 	 f.write(container_user_co)
 	 f.write("\n")
+	 f_st.write(container_user_co_st)
+	 f_st.write("\n")
 	f= open("re.txt", "r")
+	f_st= open("re_st.txt", "r")
 	container_user_co_f = f.read()
-	table = [[images_output , container_user_co_f]]
+	container_user_co_f_st = f_st.read()
+	table = [[container_user_co_f_st , images_output , container_user_co_f]]
 	return table
 
 
@@ -76,3 +83,4 @@ def output():
 if __name__ == "__main__":     
 	output()
 os.remove("re.txt")
+os.remove("re_st.txt")
