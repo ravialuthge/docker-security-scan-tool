57b5ddf65005fe64eb749257bf8d7c227069ee5f
ravialuthge <aluthgeravi@gmail.com>
2021-04-29 14:19:54 +0530

    update

--- scan.py
+++ scan.py
@@ -50,10 +50,10 @@ def container_user():
 		f = open("re.txt", "w")
 		f_st = open("re_st.txt", "w")
 		for i in (container_users):
-		if i == 'User=' or i == 'User=root':
+		 if i == 'User=' or i == 'User=root':
 				container_user_co = 'not user for the container has been created'
 				container_user_co_st = colored('WARN  ', 'red')
-		else:
+		 else:
 				container_user_co = 'user for the container has been created'
 				container_user_co_st = colored('PASS  ', 'green')
 		f.write(container_user_co)
