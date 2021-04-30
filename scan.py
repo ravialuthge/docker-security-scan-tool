55f408efc281543c45286d2b461114615899238b
ravialuthge <aluthgeravi@gmail.com>
2021-04-30 22:13:10 +0530

    update

--- scan.py
+++ scan.py
@@ -82,10 +82,8 @@ def output():
 	', 'green', attrs=['bold']))
 
 	sc_ho	= colored('Docker Host',attrs=['bold'])
-	sc_ho_1_pr	= colored('INFO   ', 'blue'), docker_version_re
-	sc_ho_1 = sc_ho_1_pr.decode("utf-8")
-	sc_ho_2_pr	= colored('WARN   ', 'red'), docker_root_re
-	sc_ho_2 = sc_ho_2_pr.decode("utf-8")
+	sc_ho_1	= colored('INFO   ', 'blue'), docker_version_re
+	sc_ho_2	= colored('WARN   ', 'red'), docker_root_re
 	sc_im	= colored('Docker Images',attrs=['bold'])
 	sc_im_1	= (tabulate(table))
 	arguments = len(sys.argv) -1
