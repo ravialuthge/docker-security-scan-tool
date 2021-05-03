94bad70afea1cdf303cf8dd2ac0c916973f7590c
ravialuthge <aluthgeravi@gmail.com>
2021-05-03 06:38:48 +0530

    update

--- scan.py
+++ scan.py
@@ -148,6 +148,47 @@ def output():
 		print (sc_im)
 		print (sc_im_1)
 		print (sc_co)
+		print (sc_co_1)
+	elif (sys.argv[1] == '-v' or sys.argv[1] == '--version') and (sys.argv[2] == '1.0.0'):
+		sub_version="1.6"
+		main_version="v1.0.0 - 04-22-2015"
+		print (banner .format(sub_version, main_version))
+		print (sc_ho)
+		print (sc_ho_1)
+		print (sc_ho_2)
+		print (sc_im)
+		print (sc_im_1)
+		print (sc_co)
+		print (sc_co_1)
+
+		sub_version_a="1.11.0"
+		print (banner .format(sub_version_a, main_version))
+		print (sc_ho)
+		print (sc_ho_1)
+		print (sc_ho_2)
+		print (sc_im)
+		print (sc_im_1)
+		print (sc_co)
+		print (sc_co_1)
+
+		sub_version_b="1.12.0"
+		print (banner .format(sub_version_b, main_version))
+		print (sc_ho)
+		print (sc_ho_1)
+		print (sc_ho_2)
+		print (sc_im)
+		print (sc_im_1)
+		print (sc_co)
+		print (sc_co_1)
+
+		sub_version_c="1.13.0"
+		print (banner .format(sub_version_c, main_version))
+		print (sc_ho)
+		print (sc_ho_1)
+		print (sc_ho_2)
+		print (sc_im)
+		print (sc_im_1)
+		print (sc_co)
 		print (sc_co_1)	
 	elif ((sys.argv[1] == '-sv' or sys.argv[1] == '--sub-version') and sys.argv[2] == '1.0.0') and sys.argv[3] == '1.6':
 		sub_version="1.6"
@@ -160,8 +201,30 @@ def output():
 		print (sc_im_1)
 		print (sc_co)
 		print (sc_co_1)
-	elif (sys.argv[1] == '-v' or sys.argv[1] == '--version') and (sys.argv[2] == '1.0.0'):
-		sub_version="1.0.0"
+	elif ((sys.argv[1] == '-sv' or sys.argv[1] == '--sub-version') and sys.argv[2] == '1.0.0') and sys.argv[3] == '1.11.0':
+		sub_version="1.11.0"
+		main_version="v1.0.0 - 04-22-2015"
+		print (banner .format(sub_version, main_version))
+		print (sc_ho)
+		print (sc_ho_1)
+		print (sc_ho_2)
+		print (sc_im)
+		print (sc_im_1)
+		print (sc_co)
+		print (sc_co_1)
+	elif ((sys.argv[1] == '-sv' or sys.argv[1] == '--sub-version') and sys.argv[2] == '1.0.0') and sys.argv[3] == '1.12.0':
+		sub_version="1.12.0"
+		main_version="v1.0.0 - 04-22-2015"
+		print (banner .format(sub_version, main_version))
+		print (sc_ho)
+		print (sc_ho_1)
+		print (sc_ho_2)
+		print (sc_im)
+		print (sc_im_1)
+		print (sc_co)
+		print (sc_co_1)
+	elif ((sys.argv[1] == '-sv' or sys.argv[1] == '--sub-version') and sys.argv[2] == '1.0.0') and sys.argv[3] == '1.13.0':
+		sub_version="1.13.0"
 		main_version="v1.0.0 - 04-22-2015"
 		print (banner .format(sub_version, main_version))
 		print (sc_ho)
