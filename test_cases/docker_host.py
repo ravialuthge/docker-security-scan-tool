e9298eeefde2100d036b8ffbbade383623d8212a
ravialuthge <aluthgeravi@gmail.com>
2021-06-01 21:16:29 +0530

    update

--- test_cases/docker_host.py
+++ test_cases/docker_host.py
@@ -51,12 +51,14 @@ def kernel_version():
 def trusted_users():
 	
 	trusted_users_cmd = "cat /etc/group | grep docker"
+	trusted_users_cmd_a = "cat /etc/group | grep docker | awk '{print $3}'"
 	health_ch_output_output = os.popen(trusted_users_cmd).read()
 	trusted_users_cmd_str = re.split(':',health_ch_output_output)
 	trusted_users = trusted_users_cmd_str[3]
 
-	if trusted_users == '':
+	if trusted_users_cmd_a == '':
 		trusted_users_re = colored('PASS   ', 'green') + "allowed trusted users to control Docker daemon"
 	else:
-		trusted_users_re = colored('WARN   ', 'red') + "Only allow trusted users to control Docker daemon"
+		trusted_users_re = colored('WARN   ', 'red') + "Only allow trusted users to control Docker daemon" + trusted_users
 	return trusted_users_re
+
