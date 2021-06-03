5c216124ec7b8ed4a9219cced0d9c919b6e9f071
ravialuthge <aluthgeravi@gmail.com>
2021-06-03 17:19:22 +0530

    update

--- scan.py
+++ scan.py
@@ -17,7 +17,6 @@ def output():
 	docker_root_re = test_cases.docker_host.docker_root()
 	table = test_cases.docker_images.container_user()
 	table_he = test_cases.docker_containers.health_check()
-	container_he_co = test_cases.docker_containers.health_check()
 	kernel_version_re = test_cases.docker_host.kernel_version()
 	trusted_users_re =  test_cases.docker_host.trusted_users()
 	full_cmd_arguments = sys.argv
@@ -37,7 +36,6 @@ def output():
 	sc_im	= (colored('Docker Images',attrs=['bold']))
 	sc_im_1	= (tabulate(table))
 	sc_co   = (colored('Docker Containers',attrs=['bold']))
-	sc_co_1_1 = (container_he_co)
 	sc_co_1 = (tabulate(table_he))
 	plugin_1 = outputpl(plugins=[dockerdatadirscan(),dockeruserscan()])
 
@@ -55,7 +53,6 @@ def output():
 		print (sc_im)
 		print (sc_im_1)
 		print (sc_co)
-		print (sc_co_1_1)
 		print (sc_co_1)
 		
 	elif (sys.argv[1] == '-p' or sys.argv[1] == '--plugin') and sys.argv[2] == 'testplugin':
