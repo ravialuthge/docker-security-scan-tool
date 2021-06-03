f0d123c3218f942d64c3b79fe7ff86dd135b2ed3
ravialuthge <aluthgeravi@gmail.com>
2021-06-03 14:55:42 +0530

    update

--- plugins/common.py
+++ plugins/common.py
@@ -8,8 +8,7 @@ class outputpl:
         test.test_modules = [banner()]
         test.test_plugins = plugins
 
+    def run(test):
         modules_to_execute = test.test_modules + test.test_plugins
         for module in modules_to_execute:
             module.scan()
-    
-        print ()
