cef0a7a79ab9905bea69e7848a06f2211d4b7c8d
ravialuthge <aluthgeravi@gmail.com>
2021-06-05 15:08:34 +0530

    update

--- test_cases/docker_containers.py
+++ test_cases/docker_containers.py
@@ -41,5 +41,37 @@ def health_check():
 		table_he = [[health_ch_co_f_st , container_image_output , container_name_output , health_ch_co_f]]
 		return table_he
 
+def apparmor():
+	images_cmd =  "docker images --format '{{ .Repository }}:{{ .Tag }}'"
+	apparmor_cmd = "docker ps -q | xargs docker inspect --format '{{ .Id }}: AppArmorProfile={{ .AppArmorProfile }}'"
+	images_ch_cmd = "docker images -q  0> /dev/null"
+	f_app = open("re_apparmor.txt", "w")
+	f_st_app = open("re_st_apparmor.txt", "w")
+	if os.popen(images_ch_cmd).read() == "":
+		container_user_co = 'images not found'
+	else:
+		apparmor_output = os.popen(apparmor_cmd).read()
+		images_output = os.popen(images_cmd).read()
+	
+		apparmor_profile = apparmor_output.split()
+		
+		for i in (apparmor_profile):
+		 if i == 'AppArmorProfile=':
+				apparmor_co = 'Verify AppArmor Profile'
+				apparmor_co_st = colored('WARN  ', 'red')
+		 else:
+				apparmor_co = 'AppArmor Profile available'
+				apparmor_co_st = colored('PASS  ', 'green')
+		 f_app.write(apparmor_co)
+		 f_app.write("\n")
+		 f_st_app.write(apparmor_co_st)
+		 f_st_app.write("\n")
+		f_app= open("re.txt", "r")
+		f_st_app= open("re_st.txt", "r")
+		apparmor_co_f = f_app.read()
+		apparmor_co_f_st = f_st_app.read()
+		table_apparmor = [[apparmor_co_f_st , images_output , apparmor_co_f]]
+		return table_apparmor
+
 
 	
