1e5e0e53b2d97fed17d470ecf85ce70967588ab2
ravialuthge <aluthgeravi@gmail.com>
2021-06-05 11:53:57 +0530

    update

--- test_cases/docker_images.py
+++ test_cases/docker_images.py
@@ -41,6 +41,8 @@ def update_ins():
 	images_ch_cmd = "docker images -q  0> /dev/null"
 	f_up = open("re_up.txt", "w")
 	f_st_up = open("re_st_up.txt", "w")
+	f_up_a = open("re_up_a.txt", "w")
+	
 	if os.popen(images_ch_cmd).read() == "":
 		images_ch_co = 'images not found'
 	else:
@@ -52,21 +54,24 @@ def update_ins():
 		for im in (images):
 			update_ins_cmd = 'docker history ' + im + " | grep -e 'update'"
 			update_ins_output = os.popen(update_ins_cmd).read()
-			update_instruction = update_ins_output.split()
-			for i in (update_instruction):
-				if i == '':
-						update_instruction_co = 'Ensure update instructions are not used alone in the Dockerfile'
-						update_instruction_co_st = colored('INFO  ', 'blue')
-				else:
-						update_instruction_co = 'update instructions are used in the Dockerfile'
-						update_instruction_co_st = colored('PASS  ', 'green')
-				f_up.write(update_instruction_co)
-				f_up.write("\n")
-				f_st_up.write(update_instruction_co_st)
-				f_st_up.write("\n")
-			f_up= open("re_up.txt", "r")
-			f_st_up= open("re_st_up.txt", "r")
-			update_instruction_co_f = f_up.read()
-			update_instruction_co_f_st = f_st_up.read()
+			f_up_a.write(update_ins_output)
+			f_up_a.write("\n")
+		f_up_a = open("re_up_a.txt", "r")
+		update_instruction = f_up_a.split()
+		for i in (update_instruction):
+			if i == '':
+					update_instruction_co = 'Ensure update instructions are not used alone in the Dockerfile'
+					update_instruction_co_st = colored('INFO  ', 'blue')
+			else:
+					update_instruction_co = 'update instructions are used in the Dockerfile'
+					update_instruction_co_st = colored('PASS  ', 'green')
+			f_up.write(update_instruction_co)
+			f_up.write("\n")
+			f_st_up.write(update_instruction_co_st)
+			f_st_up.write("\n")
+		f_up= open("re_up.txt", "r")
+		f_st_up= open("re_st_up.txt", "r")
+		update_instruction_co_f = f_up.read()
+		update_instruction_co_f_st = f_st_up.read()
 		update_instruction_table = [[update_instruction_co_f_st , images_output , update_instruction_co_f]]
 		return update_instruction_table
