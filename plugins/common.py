55c6db5bc0c7888679938317ae852ea05ee3688d
ravialuthge <aluthgeravi@gmail.com>
2021-06-06 19:33:49 +0530

    update

--- plugins/common.py
+++ plugins/common.py
@@ -6,6 +6,6 @@ class outputpl:
         test.test_plugins = plugins
 
     def run(test):
-        modules_run = test.banner_out + test.test_plugins
+        modules_run = test.test_plugins
         for module in modules_run:
             module.scan()
