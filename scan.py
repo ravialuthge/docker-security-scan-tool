ad44ac05d22b195a0edc22835ae0cd9f1125391d
ravialuthge <aluthgeravi@gmail.com>
2021-06-07 22:30:15 +0530

    update

--- scan.py
+++ scan.py
@@ -15,14 +15,14 @@ from plugins.common import outputpl
 from test_cases.process import cis_version
 from test_cases.docker_host import cis_version_120
 from test_cases.docker_images import cis_version_image_120
+from test_cases.docker_containers import cis_version_containers_120
 
 def output():
 	
 
 	
 
-	table_he = test_cases.docker_containers.health_check()
-	table_apparmor = test_cases.docker_containers.apparmor()
+
 	
 	full_cmd_arguments = sys.argv
 
@@ -36,10 +36,10 @@ def output():
 	sc_ho	= (colored('Docker Host',attrs=['bold']))
 	sc_im	= (colored('Docker Images',attrs=['bold']))
 	sc_co   = (colored('Docker Containers',attrs=['bold']))
-	sc_co_1 = (tabulate(table_he))
-	sc_co_2 = (tabulate(table_apparmor))
+	
 	sc_ho_plugin_120 = cis_version(version_plugins=[cis_version_120()])
 	sc_im_plugin_120 = cis_version(version_plugins=[cis_version_image_120()])
+	sc_co_plugin_120 = cis_version(version_plugins=[cis_version_containers_120()])
 
 
 	arguments = len(sys.argv) -1
@@ -52,8 +52,7 @@ def output():
 		print (sc_im)
 		sc_im_plugin_120.version_run()
 		print (sc_co)
-		print (sc_co_1)
-		print (sc_co_2)
+		sc_co_plugin_120.version_run()
 		
 	elif (sys.argv[1] == '-p' or sys.argv[1] == '--plugin') and sys.argv[2] == 'testplugin':
 		sc_ho_plugin_120.version_run()
@@ -66,7 +65,7 @@ def output():
 		print (sc_im)
 		
 		print (sc_co)
-		print (sc_co_1)
+	
 	elif (sys.argv[1] == '-v' or sys.argv[1] == '--version') and (sys.argv[2] == '1.0.0'):
 		sub_version="1.6"
 		main_version="v1.0.0 - 04-22-2015"
@@ -76,7 +75,7 @@ def output():
 		print (sc_im)
 	
 		print (sc_co)
-		print (sc_co_1)
+		
 
 		sub_version_a="1.11.0"
 		print (banner .format(sub_version_a, main_version))
@@ -85,7 +84,7 @@ def output():
 		print (sc_im)
 	
 		print (sc_co)
-		print (sc_co_1)
+		
 
 		sub_version_b="1.12.0"
 		print (banner .format(sub_version_b, main_version))
@@ -94,7 +93,7 @@ def output():
 		print (sc_im)
 	
 		print (sc_co)
-		print (sc_co_1)
+	
 
 		sub_version_c="1.13.0"
 		print (banner .format(sub_version_c, main_version))
@@ -103,7 +102,7 @@ def output():
 		print (sc_im)
 	
 		print (sc_co)
-		print (sc_co_1)	
+	
 	elif ((sys.argv[1] == '-sv' or sys.argv[1] == '--sub-version') and sys.argv[2] == '1.0.0') and sys.argv[3] == '1.6':
 		sub_version="1.6"
 		main_version="v1.0.0 - 04-22-2015"
@@ -113,7 +112,7 @@ def output():
 		print (sc_im)
 	
 		print (sc_co)
-		print (sc_co_1)
+
 	elif ((sys.argv[1] == '-sv' or sys.argv[1] == '--sub-version') and sys.argv[2] == '1.0.0') and sys.argv[3] == '1.11.0':
 		sub_version="1.11.0"
 		main_version="v1.0.0 - 04-22-2015"
@@ -123,7 +122,7 @@ def output():
 		print (sc_im)
 	
 		print (sc_co)
-		print (sc_co_1)
+
 	elif ((sys.argv[1] == '-sv' or sys.argv[1] == '--sub-version') and sys.argv[2] == '1.0.0') and sys.argv[3] == '1.12.0':
 		sub_version="1.12.0"
 		main_version="v1.0.0 - 04-22-2015"
@@ -133,7 +132,7 @@ def output():
 		print (sc_im)
 
 		print (sc_co)
-		print (sc_co_1)
+	
 	elif ((sys.argv[1] == '-sv' or sys.argv[1] == '--sub-version') and sys.argv[2] == '1.0.0') and sys.argv[3] == '1.13.0':
 		sub_version="1.13.0"
 		main_version="v1.0.0 - 04-22-2015"
@@ -143,7 +142,7 @@ def output():
 		print (sc_im)
 	
 		print (sc_co)
-		print (sc_co_1)
+	
 	elif (sys.argv[1] == '-s' or sys.argv[1] == '--scan') and sys.argv[2] == 'host':
 		sub_version="1.2.0"
 		main_version="v1.2.0 - 07-29-2019"
@@ -161,7 +160,7 @@ def output():
 		main_version="v1.2.0 - 07-29-2019"
 		print (banner .format(sub_version, main_version))
 		print (sc_co)
-		print (sc_co_1)
+
 	elif (sys.argv[1] == '-h' or sys.argv[1] == '--help'):
 		print ("help")
 	else:
