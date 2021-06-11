b61963ca69ae1436c5b325a6fe1f749392559172
ravialuthge <aluthgeravi@gmail.com>
2021-06-11 15:25:34 +0530

    update

--- plugins/dockeruser.py
+++ plugins/dockeruser.py
@@ -7,7 +7,8 @@ class dockeruserscan:
     def scan(test):
         trusted_users_cmd = "cat /etc/group | grep docker"
         health_ch_output_output = os.popen(trusted_users_cmd).read()
-        trusted_users_cmd_str = re.split(':',health_ch_output_output)
+        health_ch_output_output_a = health_ch_output_output.rstrip()
+        trusted_users_cmd_str = re.split(':',health_ch_output_output_a)
         trusted_users_output_a = trusted_users_cmd_str[3]
         trusted_users_output = "user=" + trusted_users_output_a
 
