3094d371e8af83470d7e7ada9f84870ebff77cad
ravialuthge <aluthgeravi@gmail.com>
2021-06-17 22:31:11 +0530

    update

---
+++ plugins/contenttrust.py
@@ -0,0 +1,15 @@
+import os
+import re
+from termcolor import colored
+
+class contenttrust:
+	def scan(test):
+		contenttrust_cmd = "1"
+		contenttrust_version_cmd = "echo $DOCKER_CONTENT_TRUST"
+		contenttrust_output = os.popen(contenttrust_version_cmd).read()
+
+		if contenttrust_output == contenttrust_cmd:
+			contenttrust_re = colored('PASS   ', 'green') + "Enabled Content trust for Docker"
+		else:
+			contenttrust_re = colored('WARN   ', 'red') + "Enable Content trust for Docker"
+		print (contenttrust_re)
