d529a0aa05cf982fc817b089a45a8a42b4a0011a
ravialuthge <aluthgeravi@gmail.com>
2021-06-19 17:45:13 +0530

    update

--- plugins/encryptnet.py
+++ plugins/encryptnet.py
@@ -9,7 +9,7 @@ class encryptnet:
         f_encryptnet = open("re_encryptnet.txt", "w")
         f_st_encryptnet = open("re_st_encryptnet.txt", "w")
         encryptnet_ch_cmd = "docker network ls --filter driver=overlay --quiet | xargs docker network inspect --format '{{.Name}}' | grep -v 'dtr\|interlock map\|ingress'"
-        encryptnet_ch_en_cmd = "docker network ls --filter driver=overlay --quiet | xargs docker network inspect --format '{{ .Options }}' | grep -v 'dtr\|interlock map\|ingress map'"
+        encryptnet_ch_en_cmd = "docker network ls --filter driver=overlay --quiet | xargs docker network inspect --format '{{.Name}} {{ .Options }}' | grep -v 'dtr\|interlock map\|ingress map'"
         
         encryptnet_ch_output = os.popen(encryptnet_ch_cmd).read()
         encryptnet_ch_en_output = os.popen(encryptnet_ch_en_cmd).read()
