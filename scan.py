c19d72a7c4479a55081f118af03fc31e50fdab52
ravialuthge <aluthgeravi@gmail.com>
2021-06-26 08:33:55 +0530

    update

--- scan.py
+++ scan.py
@@ -52,7 +52,7 @@ def output():
 	parser.add_argument("-sv", "--sub-version", type=str , help="run for sub CIS versions  (currently available 1.0.0 sub versions 1.6, 1.11.0, 1.12.0, 1.13.0)")
 	parser.add_argument("-p", "--profile", type=str, help="run for configuration profiles  (currently available docker host , docker images & docker containers)")
 	args = parser.parse_args()
-	if args.version == '1.2.0' or args:
+	if args:
 		sub_version="1.2.0"
 		main_version="v1.2.0 - 07-29-2019"
 		print (banner .format(sub_version, main_version))
@@ -62,7 +62,7 @@ def output():
 		sc_im_plugin_120.version_run()
 		print (sc_co)
 		sc_co_plugin_120.version_run()
-	elif args.version == '1.1.0':
+
 		sub_version="1.1.0"
 		main_version="v1.1.0 - 07-06-2017"
 		print (banner .format(sub_version, main_version))
@@ -71,7 +71,7 @@ def output():
 		print (sc_im)
 		
 		print (sc_co)
-	elif args.version == "1.0.0" and args.sub_version == "1.6":
+
 		sub_version="1.6"
 		main_version="v1.0.0 - 04-22-2015"
 		print (banner .format(sub_version, main_version))
@@ -81,7 +81,7 @@ def output():
 		sc_im_plugin_16.version_run()
 		print (sc_co)
 		sc_co_plugin_16.version_run()
-	elif args.version == "1.0.0" and args.sub_version == "1.11.0":
+
 		sub_version="1.11.0"
 		main_version="v1.0.0 - 04-22-2015"
 		print (banner .format(sub_version, main_version))
@@ -91,7 +91,7 @@ def output():
 		sc_im_plugin_111.version_run()
 		print (sc_co)
 		sc_co_plugin_111.version_run()
-	elif args.version == "1.0.0" and args.sub_version == "1.12.0":
+
 		sub_version="1.12.0"
 		main_version="v1.0.0 - 04-22-2015"
 		print (banner .format(sub_version, main_version))
@@ -101,7 +101,7 @@ def output():
 		sc_im_plugin_112.version_run()
 		print (sc_co)
 		sc_co_plugin_112.version_run()
-	elif args.version == "1.0.0" and args.sub_version == "1.13.0":
+
 		sub_version="1.13.0"
 		main_version="v1.0.0 - 04-22-2015"
 		print (banner .format(sub_version, main_version))
@@ -110,25 +110,6 @@ def output():
 		print (sc_im)
 	
 		print (sc_co)
-	elif args.profile == 'host':
-		sub_version="1.2.0"
-		main_version="v1.2.0 - 07-29-2019"
-		print (banner .format(sub_version, main_version))
-		print (sc_ho)
-		sc_ho_plugin_120.version_run()
-	elif args.profile == 'images':
-		sub_version="1.2.0"
-		main_version="v1.2.0 - 07-29-2019"
-		print (banner .format(sub_version, main_version))
-		print (sc_im)
-		sc_im_plugin_120.version_run()
-	elif args.profile == 'containers':
-		sub_version="1.2.0"
-		main_version="v1.2.0 - 07-29-2019"
-		print (banner .format(sub_version, main_version))
-		print (sc_co)
-		sc_co_plugin_120.version_run()
-
 
 if __name__ == "__main__":     
 	output()
