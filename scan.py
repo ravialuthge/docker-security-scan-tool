4f1dcce95aa2563c44b999e4b3e41ccf6f75c0d5
ravialuthge <aluthgeravi@gmail.com>
2021-06-30 22:04:04 +0530

    update

--- scan.py
+++ scan.py
@@ -1,10 +1,10 @@
 import os
 import sys
 from termcolor import colored
-#from profiles.process import *
-#from profiles.docker_host import *
-#from profiles.docker_images import *
-#from profiles.docker_containers import *
+from profiles.process import *
+from profiles.docker_host import *
+from profiles.docker_images import *
+from profiles.docker_containers import *
 import argparse
 
 
