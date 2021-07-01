28c26d0f5a305aee1fd26638f6f55f66a1685583
ravialuthge <aluthgeravi@gmail.com>
2021-07-01 07:32:26 +0530

    update

--- abc.py
+++ abc.py
@@ -5,8 +5,7 @@ def load_all_modules_from_dir(dirname):
     for importer, package_name, _ in pkgutil.iter_modules([dirname]):
         full_package_name = '%s.%s' % (dirname, package_name)
         if full_package_name not in sys.modules:
-            module = importer.find_module(package_name
-                        ).load_module(full_package_name)
+            module = importer.find_module(package_name).load_module(full_package_name)
             print (module)
 
 load_all_modules_from_dir('profiles')
