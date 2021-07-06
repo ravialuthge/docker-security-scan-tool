b36072f663e3a2851c43201767a1cf663a2d5e5a
ravialuthge <aluthgeravi@gmail.com>
2021-07-06 21:51:31 +0530

    update

--- profiles/docker_images.py
+++ profiles/docker_images.py
@@ -1,25 +1,25 @@
-from plugins.containeruser import *
-from plugins.updateins import *
-from plugins.contenttrust import *
-from plugins.common import *
-#from plugins import *
+#from plugins.containeruser import *
+#from plugins.updateins import *
+#from plugins.contenttrust import *
+#from plugins.common import *
+from plugins import *
 
 class cis_version_image_120:
 	def version_scan(version):
-		plugin_images_120 = outputpl(plugins=[containeruser(),updateins()])
+		plugin_images_120 = common.outputpl(plugins=[containeruser.containeruser(),updateins.updateins()])
 		plugin_images_120.run()
 
 class cis_version_image_16:
 	def version_scan(version):
-		plugin_images_16 = outputpl(plugins=[containeruser()])
+		plugin_images_16 = common.outputpl(plugins=[containeruser.containeruser()])
 		plugin_images_16.run()
 
 class cis_version_image_111:
 	def version_scan(version):
-		plugin_images_111 = outputpl(plugins=[contenttrust()])
+		plugin_images_111 = common.outputpl(plugins=[contenttrust.contenttrust()])
 		plugin_images_111.run()
 
 class cis_version_image_112:
 	def version_scan(version):
-		plugin_images_112 = outputpl(plugins=[updateins()])
+		plugin_images_112 = common.outputpl(plugins=[updateins.updateins()])
 		plugin_images_112.run()
