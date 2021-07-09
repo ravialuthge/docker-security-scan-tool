238f5137ab3b9cb92d8a1ae0e7f135ffece9d726
ravialuthge <aluthgeravi@gmail.com>
2021-07-09 23:10:23 +0530

    update

--- plugins/officialimage.py
+++ plugins/officialimage.py
@@ -1,1 +1,11 @@
+p=(input("enter Dockerfile path :"))
+
+f = open(p, "r")
+mystring  = f.read()
+
+for item in mystring.split("\n"):
+   if "FROM" in item:
+      print (item.strip())
+
+
 docker search --format "{{.IsOfficial}}" --filter is-official=true redis
