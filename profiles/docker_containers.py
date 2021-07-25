e98000360f81157ea4fdaabc04d16362539fbc5e
ravialuthge <aluthgeravi@gmail.com>
2021-07-25 11:12:51 +0530

    update

--- profiles/docker_containers.py
+++ profiles/docker_containers.py
@@ -3,12 +3,12 @@ import sdk.containers as containers
 
 class cis_version_containers_120:
 	def version_scan(version):
-		plugin_containers_120 = common.outputpl(plugins=[healthcheck.healthcheck(),apparmor.apparmor(containers.containerlist)])
+		plugin_containers_120 = common.outputpl(plugins=[healthcheck.healthcheck(),apparmor.apparmor()])
 		plugin_containers_120.run()
 
 class cis_version_containers_16:
 	def version_scan(version):
-		plugin_containers_16 = common.outputpl(plugins=[apparmor.apparmor(containers.containerlist)])
+		plugin_containers_16 = common.outputpl(plugins=[apparmor.apparmor()])
 		plugin_containers_16.run()
 
 class cis_version_containers_111:
