faa6ebc93260573c2bf173885fbafa9c308f6475
ravialuthge <aluthgeravi@gmail.com>
2021-07-27 08:26:10 +0530

    update

--- plugins/apparmor.py
+++ plugins/apparmor.py
@@ -7,14 +7,16 @@ from sdk.containers_list import *
 class apparmor(containerlist):
     """Verify AppArmor Profile, if applicable"""
     def __init__(self):
+        self.lst_con_img=[]
+        self.lst_con_apparmor=[]
+        self.lst_apparmor_co=[]
+        self.lst_apparmor_co_st=[]
+    
+    def apparmor_scan(self):
         super().__init__()
         if self.lst == "[]":
             print ('containers not running')
         else:
-            lst_con_img=[]
-            lst_con_apparmor=[]
-            lst_apparmor_co=[]
-            lst_apparmor_co_st=[]
             client = docker.from_env()
             for container in client.containers.list():
                 a = container.image
@@ -23,8 +25,8 @@ class apparmor(containerlist):
                 bb = b[1]
                 bbc = bb.replace(">",'')
                 vv = bbc.replace("'",'')
-                lst_con_img.append(vv)
-            lst_con_img_a = "\n".join(lst_con_img)
+                self.lst_con_img.append(vv)
+            lst_con_img_a = "\n".join(self.lst_con_img)
             images_output = lst_con_img_a
             images = self.lst
             for im in (images):
@@ -32,21 +34,21 @@ class apparmor(containerlist):
                 apparmor_output = os.popen(apparmor_cmd).read()
                 apparmor_profile = apparmor_output.rstrip()
                 apparmor_profile_str = str(apparmor_profile)
-                lst_con_apparmor.append(apparmor_profile_str)
-            apparmor_profile_str_a_s = lst_con_apparmor
+                self.lst_con_apparmor.append(apparmor_profile_str)
+            apparmor_profile_str_a_s = self.lst_con_apparmor
             for i in (apparmor_profile_str_a_s):
                 if i == 'AppArmorProfile=':
                         apparmor_co = 'Verify AppArmor Profile, if applicable'
                         apparmor_co_st = colored('WARN  ', 'red')
-                        lst_apparmor_co.append(apparmor_co)
-                        lst_apparmor_co_st.append(apparmor_co_st)
+                        self.lst_apparmor_co.append(apparmor_co)
+                        self.lst_apparmor_co_st.append(apparmor_co_st)
                 else:
                         apparmor_co = 'AppArmor Profile available'
                         apparmor_co_st = colored('PASS  ', 'green')
-                        lst_apparmor_co.append(apparmor_co)                      
-                        lst_apparmor_co_st.append(apparmor_co_st)
-            f_app = "\n".join(lst_apparmor_co)
-            f_st_app = "\n".join(lst_apparmor_co_st)
+                        self.lst_apparmor_co.append(apparmor_co)                      
+                        self.lst_apparmor_co_st.append(apparmor_co_st)
+            f_app = "\n".join(self.lst_apparmor_co)
+            f_st_app = "\n".join(self.lst_apparmor_co_st)
             apparmor_co_f = f_app
             apparmor_co_f_st = f_st_app
             table_apparmor = [[apparmor_co_f_st , images_output , apparmor_co_f]]
