962842d2b210c2917c9736413d8b202a24a8ec12
ravialuthge <aluthgeravi@gmail.com>
2021-07-29 14:48:57 +0530

    update

--- profiles/docker_containers.py
+++ profiles/docker_containers.py
@@ -4,9 +4,11 @@ class cis_version_containers(apparmor):
 	def version_120(self):
 		plugin_containers_120 = apparmor()
 		plugin_containers_120_output = plugin_containers_120.apparmor_scan()
+		return plugin_containers_120_output
 
 object1 = cis_version_containers()
 sum = object1.version_120()
+print (sum)
 
 
 
