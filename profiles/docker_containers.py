c94e3360450fb618a4af81e19a13cf8a47076ac5
ravialuthge <aluthgeravi@gmail.com>
2021-07-30 19:00:23 +0530

    update

---
+++ profiles/docker_containers.py
@@ -0,0 +1,26 @@
+from plugins import *
+
+class cis_version_containers_120:
+	def version_scan(version):
+		plugin_containers_120 = common.outputpl(plugins=[healthcheck.healthcheck(),apparmor.apparmor()])
+		plugin_containers_120.run()
+
+class cis_version_containers_16:
+	def version_scan(version):
+		plugin_containers_16 = common.outputpl(plugins=[apparmor.apparmor()])
+		plugin_containers_16.run()
+
+class cis_version_containers_111:
+	def version_scan(version):
+		plugin_containers_111 = common.outputpl(plugins=[seccomp.seccomp()])
+		plugin_containers_111.run()
+
+class cis_version_containers_112:
+	def version_scan(version):
+		plugin_containers_112 = common.outputpl(plugins=[healthcheck.healthcheck()])
+		plugin_containers_112.run()
+
+
+
+
+	
