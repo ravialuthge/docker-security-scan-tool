9e80ae662181b5715da6ba624322e5a365fe433d
ravialuthge <aluthgeravi@gmail.com>
2021-08-01 18:58:52 +0530

    update

--- scan.py
+++ scan.py
@@ -9,6 +9,12 @@ import textwrap
 import importlib
 #from plugins.apparmor import *
 
+for module in os.listdir(os.path.plugins(__file__)):
+    if module == '__init__.py' or module[-3:] != '.py':
+        continue
+    __import__(module[:-3], locals(), globals())
+
+
 def iter_namespace(ns_pkg):
 	return pkgutil.iter_modules(ns_pkg.__path__, ns_pkg.__name__ + ".")
 
@@ -135,6 +141,7 @@ def output():
 			
 if __name__ == "__main__": 
 	load_plugins() 
+	del module
 	output()
 
 pwd_output = os.getcwd()
