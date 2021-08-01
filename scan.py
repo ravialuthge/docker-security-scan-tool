e62f4a377fdffdc4353d3acb5d78aef6b008d7c0
ravialuthge <aluthgeravi@gmail.com>
2021-08-01 08:53:57 +0530

    update

--- scan.py
+++ scan.py
@@ -42,6 +42,9 @@ def output():
 
 	sc_dockerfile = (colored('Best practices for writing Dockerfiles',attrs=['bold']))
 
+	object1 = apparmor()
+	output = object1.apparmor_scan()
+
 	arguments_a = len(sys.argv) -1
 	if arguments_a == 0:
 		sub_version="1.2.0"
