b145f86db220d06a4db22b52fd82b1a60a69bb8e
ravialuthge <aluthgeravi@gmail.com>
2021-08-01 08:55:23 +0530

    update

--- scan.py
+++ scan.py
@@ -12,7 +12,7 @@ def iter_namespace(ns_pkg):
 	return pkgutil.iter_modules(ns_pkg.__path__, ns_pkg.__name__ + ".")
 
 def load_plugins():
-    for _, name, _ in iter_namespace(profiles):
+    for _, name, _ in iter_namespace(plugins):
         importlib.import_module(name)
 
 discovered_plugins = {
