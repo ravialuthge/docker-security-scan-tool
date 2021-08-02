eea30ffba58d1a28c8e922192aa7e006e77e32c5
ravialuthge <aluthgeravi@gmail.com>
2021-08-02 22:15:09 +0530

    update

--- scan.py
+++ scan.py
@@ -9,7 +9,7 @@ import textwrap
 import importlib
 from plugins import *
 
-__file__ = plugins
+module = plugins
 for module in os.listdir(os.path.dirname(__file__)):
     if module == '__init__.py' or module[-3:] != '.py':
         continue
