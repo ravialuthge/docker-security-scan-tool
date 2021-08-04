26fb2d4c1cc4c8e2b3730ac602b14a3bfee96b1e
ravialuthge <aluthgeravi@gmail.com>
2021-08-04 07:08:21 +0530

    update

--- scan.py
+++ scan.py
@@ -13,7 +13,9 @@ def load_all_modules_from_dir(dirname):
     for importer, package_name, _ in pkgutil.iter_modules([dirname]):
         full_package_name = '%s.%s' % (dirname, package_name)
         if full_package_name not in sys.modules:
-            __import__(importer.find_module(package_name).load_module(full_package_name))
+            module = importer.find_module(package_name
+                        ).load_module(full_package_name)
+            print (module)
 
 def iter_namespace(ns_pkg):
 	return pkgutil.iter_modules(ns_pkg.__path__)
