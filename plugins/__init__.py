f9b08a452db54365b9c133c172fb741098be085a
ravialuthge <aluthgeravi@gmail.com>
2021-08-07 10:57:26 +0530

    update

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,4 +1,5 @@
 from os.path import dirname, basename, isfile, join
 import glob
+
 modules = glob.glob(join(dirname(__file__), "*.py"))
-__all__ = [ basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
+__all__ = [ basename(p)[:-3] for p in modules if isfile(p) and not p.endswith('__init__.py')]
