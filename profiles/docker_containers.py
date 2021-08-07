ca8ba8f559680e897d380f053651620183282cbc
ravialuthge <aluthgeravi@gmail.com>
2021-08-07 14:45:18 +0530

    update

--- profiles/docker_containers.py
+++ profiles/docker_containers.py
@@ -2,6 +2,6 @@ from plugins import *
 
 class cis_version_containers(apparmor.ApparmorPlugin):
 	def cis_version_12(test):
-		testcases = ApparmorPlugin().apparmor_scan()
+		testcases = apparmor.ApparmorPlugin().apparmor_scan()
 		print (testcases)
 
