92ca765531bab1d6903020d28b66c2dbd8f4f684
ravialuthge <aluthgeravi@gmail.com>
2021-08-22 11:51:18 +0530

    update

---
+++ sdk/images_name_list.py
@@ -0,0 +1,19 @@
+import docker
+
+class imageslist(object):
+    def __init__(test): 
+        lst=[]
+        client = docker.from_env()
+        for image in client.images.list():
+                    images_ch_cmd_a_s = image.tags
+                    images_ch_cmd_str = str(images_ch_cmd_a_s)
+                    bbc = images_ch_cmd_str.replace("[",'')
+                    bbcdr = bbc.replace("]",'')
+                    lst.append(bbcdr)
+        test.lst = lst 
+    def images(test):
+        test.lst = test.lst        
+        return test.lst
+
+sdk = imageslist().images()
+print (sdk)
