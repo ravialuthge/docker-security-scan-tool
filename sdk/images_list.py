efe6c05222418448f9025de544598c4192845b37
ravialuthge <aluthgeravi@gmail.com>
2021-09-04 12:34:39 +0530

    update

--- sdk/images_list.py
+++ sdk/images_list.py
@@ -10,14 +10,13 @@ class imageslist(object):
                     x = images_ch_cmd_a.split(":")
                     ff = x[1]
                     lst.append(ff)
-        test.lst = lst
-        for image in client.images.list():
                     images_ch_cmd_a_s = image.tags
                     images_ch_cmd_str = str(images_ch_cmd_a_s)
                     bbc = images_ch_cmd_str.replace("[",'')
                     bbcdr = bbc.replace("]",'')
                     bbcf = bbcdr.replace("'",'')
                     lst_img_name.append(bbcf)
+        test.lst = lst
         test.lst_img_name = lst_img_name
     def images_id(test):
         test.lst = test.lst        
