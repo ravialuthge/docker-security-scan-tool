d7aee3f35b13e5c53df935144fa05990475f2429
ravialuthge <aluthgeravi@gmail.com>
2021-09-05 17:45:06 +0530

    update

--- plugins/seccomp.py
+++ plugins/seccomp.py
@@ -36,7 +36,7 @@ class seccomp(containerlist):
             seccomp_ch = test.lst_health_ch
 
             for h in (seccomp_ch):
-                    if h == '[seccomp:unconfined]':
+                    if h == 'SecurityOpt=[]':
                             seccomp_ch_co = 'Do not disable default seccomp profile'
                             seccomp_ch_co_st = colored('WARN  ', 'red')
                             test.lst_seccomp_ch_co.append(seccomp_ch_co)
@@ -51,4 +51,4 @@ class seccomp(containerlist):
             seccomp_ch_co_f_st = "\n".join(test.lst_seccomp_ch_co_st)
             table_seccomp = [[seccomp_ch_co_f_st , container_image_output , container_name_output , seccomp_ch_co_f]]
             table_seccomp_out = tabulate(table_seccomp)
-            return table_seccomp_out , seccomp_ch
+            return table_seccomp_out
