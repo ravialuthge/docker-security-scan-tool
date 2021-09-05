1e5cbcd438628cb37db7be1f7d582298700996d9
ravialuthge <aluthgeravi@gmail.com>
2021-09-05 18:55:47 +0530

    update

--- profiles/docker_images.py
+++ profiles/docker_images.py
@@ -7,3 +7,6 @@ class cis_version_images():
 	def cis_version_111(test):
 		testcases = contenttrust.contenttrust().contenttrust_scan()
 		print (testcases)
+	def cis_version_112(test):
+		testcases = updateins.updateins().updateins_scan()
+		print (testcases)
