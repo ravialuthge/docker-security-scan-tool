259a0d7e177f0faed6340b3bc5dbe64f204d7f30
ravialuthge <aluthgeravi@gmail.com>
2021-09-16 06:01:12 +0530

    update

--- scan.py
+++ scan.py
@@ -17,11 +17,16 @@ discovered_plugins = {
 }
 
 lst_plugins=[]
-
+lst_plugins_help=[]
 for he in (discovered_plugins):
-		lst_plugins.append(he,str(he.__class__.__name__))
+		lst_plugins.append(he)
+		
 _lst_plugins_a = sorted(lst_plugins)
+for he_name in (_lst_plugins_a):
+	lst_plugins_help.append(str(he_name.__class__.__name__))
 lst_plugins_a = "\n".join(_lst_plugins_a)
+_lst_plugins_help = "\n".join(lst_plugins_help)
+
 
 def output():
 	
@@ -58,7 +63,7 @@ def output():
 		docker_containers.cis_version_containers().cis_version_111()
 		
 	else:
-		parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,epilog=textwrap.dedent("plugins:\n\n" + lst_plugins_a))
+		parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,epilog=textwrap.dedent("plugins:\n\n" + lst_plugins_a + _lst_plugins_help))
 		parser.add_argument("-v", "--version", type=str , help="run for main CIS versions (currently available versions 1.2.0 , 1.1.0 , 1.0.0)")
 		parser.add_argument("-sv", "--sub-version", type=str , help="run for sub CIS versions  (currently available 1.0.0 sub versions 1.6, 1.11.0, 1.12.0, 1.13.0)")
 		parser.add_argument("-pr", "--profile", type=str, help="run for configuration profiles  (currently available docker host , docker images & docker containers)")
