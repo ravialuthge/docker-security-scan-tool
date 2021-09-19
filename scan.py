605f578e4eea9cec232cb8354857297f6a0f19bc
ravialuthge <aluthgeravi@gmail.com>
2021-09-19 12:00:56 +0530

    update

--- scan.py
+++ scan.py
@@ -179,8 +179,8 @@ def output():
 			def_bbc = _def_str.replace("[",'')
 			def_bbcdr = def_bbc.replace("]",'')
 			__def = def_bbcdr.replace("'",'')
-			module = __import__('apparmor.apparmor')
-			function_string = "%s.%s.%s" % (module,__cls,__def)
+			module = __import__('apparmor')
+			function_string = module,__cls,__def
 			mod_name, func_name = function_string.rsplit('.',1)
 			mod = importlib.import_module(mod_name)
 			func = getattr(mod, func_name)
