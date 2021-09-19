795f518aa805331f3ab7933f8f3a0091157ae31e
ravialuthge <aluthgeravi@gmail.com>
2021-09-19 07:20:23 +0530

    update

--- scan.py
+++ scan.py
@@ -166,18 +166,16 @@ def output():
 				if t != '__init__':
 					_def.append(t)
 		
-			_cls_str = _cls.strip()
-			_bbc = _cls_str.replace("[",'')
-			_bbcdr = _bbc.replace("]",'')
-			__cls = _bbcdr.replace("'",'')
+			#_cls_str = _cls.strip()
+			#_bbc = _cls_str.replace("[",'')
+			#_bbcdr = _bbc.replace("]",'')
+			#__cls = _bbcdr.replace("'",'')
 			
-			_def_str = _def.strip()
-			def_bbc = _def_str.replace("[",'')
-			def_bbcdr = def_bbc.replace("]",'')
-			__def = def_bbcdr.replace("'",'')
-			testcases = "%.%().%()" % (apparmor,__cls,__def)
-			print (__cls)
-			print (__def)
+			#_def_str = _def.strip()
+			#def_bbc = _def_str.replace("[",'')
+			#def_bbcdr = def_bbc.replace("]",'')
+			#__def = def_bbcdr.replace("'",'')
+			testcases = "%.%().%()" % (apparmor,_cls,_def)
 			print (testcases)
 
 		elif args.files == "officialimage":
