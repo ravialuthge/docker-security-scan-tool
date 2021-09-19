f30abf7f6ade8ab93bb75d172fbc8abe564c63a2
ravialuthge <aluthgeravi@gmail.com>
2021-09-19 11:18:49 +0530

    update

--- scan.py
+++ scan.py
@@ -179,7 +179,7 @@ def output():
 			def_bbc = _def_str.replace("[",'')
 			def_bbcdr = def_bbc.replace("]",'')
 			__def = def_bbcdr.replace("'",'')
-			testcases = "apparmor.ApparmorPlugin().%s()" % (__def)
+			testcases = "apparmor.ApparmorPlugin().%s()" % (_def)
 			print (testcases)
 
 		elif args.files == "officialimage":
