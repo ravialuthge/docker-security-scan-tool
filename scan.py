f99efff63d4f0f35943049a04dff46ab93087a4c
ravialuthge <aluthgeravi@gmail.com>
2021-09-20 16:06:20 +0530

    update

--- scan.py
+++ scan.py
@@ -191,15 +191,15 @@ def output():
 			#function_string = "%s.%s.%s" % (module,cls_name,fun_name)
 			_function_string = "{0}.{1}.{2}"
 			function_string = _function_string .format(module,cls_name,fun_name)
-			#mod_name, func_name = function_string.rsplit('.',1)
-			#mod = importlib.import_module(mod_name)
-			#func = getattr(mod, func_name)
-			func = importlib.import_module(function_string)
-			#result = func
+			mod_name, func_name = function_string.rsplit('.',1)
+			mod = importlib.import_module(mod_name)
+			func = getattr(mod, func_name)
+			#func = importlib.import_module(function_string)
+			result = func
 			#getattr(locals().get("foo") or globals().get("foo") or __import__("foo"), "bar")()
 			#_testcases = testcases()
 			#_testcases = getattr('apparmor',__cls,__def)()
-			print (func)
+			print (result)
 		
 
 		elif args.files == "officialimage":
