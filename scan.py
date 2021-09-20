545d0d998edcd6bf8ca532e14468ab3c9b991be5
ravialuthge <aluthgeravi@gmail.com>
2021-09-20 17:49:30 +0530

    update

--- scan.py
+++ scan.py
@@ -195,7 +195,7 @@ def output():
 			#mod = importlib.import_module(mod_name)
 			#func = getattr(mod, func_name)
 			#func = importlib.import_module(function_string)
-			result = function_string()
+			result = callable(function_string)
 			#getattr(locals().get("foo") or globals().get("foo") or __import__("foo"), "bar")()
 			#_testcases = testcases()
 			#_testcases = getattr('apparmor',__cls,__def)()
