0c6c7cdbf49fba5698de8e6c7e7b48ad333564b2
ravialuthge <aluthgeravi@gmail.com>
2021-09-21 19:19:17 +0530

    update

--- plugins/officialimage.py
+++ plugins/officialimage.py
@@ -5,26 +5,27 @@ class officialimage(object):
     """Check Docker Official Image"""
     def __init__(test):
       parser = argparse.ArgumentParser()
-      parser.add_argument("path")
+      parser.add_argument("-f", "--filepath", type=str, help="Dockerfile path")
       test.args = parser.parse_args()
-      test.p = test.args.path
+      test.p = test.args.filepath
 
     def officialimagescan(test):
-       try:
-         f = open(test.p, "r")
-         mystring  = f.read()
+       if test.p:
+         try:
+            f = open(test.p, "r")
+            mystring  = f.read()
 
-         for item in mystring.split("\n"):
-            if "FROM" in item:
-               d =  item.strip()
-               s = d.split()
-               o = s[1]
-               cmd = "docker search --format '{{.IsOfficial}}' --filter is-official=true " + o
-               cmdout = os.popen(cmd).read()
-               cmdout_a = cmdout.rstrip()
-               if cmdout_a == '[OK]':
-                  print (o +" is Docker Official Image")
-               else:
-                  print (o +" not Docker Official Images")
-       except FileNotFoundError:
-          print ("I did not find the Dockerfile at, "+str(test.p))
+            for item in mystring.split("\n"):
+               if "FROM" in item:
+                  d =  item.strip()
+                  s = d.split()
+                  o = s[1]
+                  cmd = "docker search --format '{{.IsOfficial}}' --filter is-official=true " + o
+                  cmdout = os.popen(cmd).read()
+                  cmdout_a = cmdout.rstrip()
+                  if cmdout_a == '[OK]':
+                     print (o +" is Docker Official Image")
+                  else:
+                     print (o +" not Docker Official Images")
+         except FileNotFoundError:
+            print ("I did not find the Dockerfile at, "+str(test.p))
