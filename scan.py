55593b4d97bc88c5a52afc1be6a18c0c4e7c488c
ravialuthge <aluthgeravi@gmail.com>
2021-09-23 06:59:23 +0530

    update

--- scan.py
+++ scan.py
@@ -67,11 +67,11 @@ def output():
 		_def_name = []
 		_def = []
 		pattern_def = re.compile("def (.*)\(")
-		pattern = re.compile("class (.*)\(")
+		pattern_cls = re.compile("class (.*)\(")
 		for lp in _lst_plugins_a:
 			module_name = "plugins/"+lp+".py"
 			for i,line in enumerate(open(module_name)):
-				for match in re.finditer(pattern,line):
+				for match in re.finditer(pattern_cls,line):
 					cls = '%s' % (match.groups()[0])
 					_cls.append(cls)
 				for match in re.finditer(pattern_def,line):
@@ -86,15 +86,13 @@ def output():
 			def_bbcdr = def_bbc.replace("]",'')
 			__def = def_bbcdr.replace("'",'')
 			fun_name = __def
-			for mo in _lst_plugins_a:
-				if mo == _module_name:
-					for cl in _cls:
-						_mod = "plugins."+mo
-						mod = importlib.import_module(_mod)
-						class_name = cl
-						my_class = getattr(mod, class_name)()
-						result = getattr(my_class, "%s" % (fun_name))()
-						print (result)
+			for cl in _cls:
+				_mod = "plugins."+_module_name
+				mod = importlib.import_module(_mod)
+				class_name = cl
+				my_class = getattr(mod, class_name)()
+				result = getattr(my_class, "%s" % (fun_name))()
+				print (result)
 		
 		#print (sc_ho)
 		#docker_host.cis_version_host().cis_version_112()
