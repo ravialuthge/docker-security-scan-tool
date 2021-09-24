664ea818141410f1dfe1e9fb3a893ed74ed0922a
ravialuthge <aluthgeravi@gmail.com>
2021-09-24 22:49:02 +0530

    update

--- scan.py
+++ scan.py
@@ -155,7 +155,7 @@ def output():
 		parser.add_argument("-v", "--version", type=str , help="run for main CIS versions (currently available versions 1.2.0 , 1.1.0 , 1.0.0)")
 		parser.add_argument("-sv", "--subversion", type=str , help="run for sub CIS versions  (currently available 1.0.0 sub versions 1.6, 1.11.0, 1.12.0, 1.13.0)")
 		parser.add_argument("-cp", "--conprofile", type=str, help="run for configuration profiles  (currently available docker host , docker images & docker containers)")
-		parser.add_argument("-f", "--files",dest = "file_path" ,type=str, help="check Best practices for Dockerfiles & docker-compose file")
+		parser.add_argument("-f", "--files",dest = "file_path" , default = None , help="check Best practices for Dockerfiles & docker-compose file")
 		#parser.add_argument("-i", "--id", type=str, help="run for docker image id & docker container id")
 		parser.add_argument("-p", "--plugins", type=str, help="for individually run plugins")
 		#parser.add_argument("-pa", "--path", dest="filename", required=True,help="input file", metavar="FILE")
