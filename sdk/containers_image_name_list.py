11fcc0d226b114a477128195efdac1c52b591c94
ravialuthge <aluthgeravi@gmail.com>
2021-09-27 21:51:02 +0530

    update

---
+++ sdk/containers_image_name_list.py
@@ -0,0 +1,14 @@
+import docker
+
+class containerimagelist(object):
+    def __init__(test):
+        con_img_lst=[]
+        client = docker.from_env()
+        for container in client.containers.list():
+                    container_image_list = container.image.tags
+                    for ci in container_image_list:
+                        con_img_lst.append(ci)
+        test.con_img_lst = con_img_lst
+    def container(test):
+        test.con_img_lst = test.con_img_lst
+        return test.con_img_lst
