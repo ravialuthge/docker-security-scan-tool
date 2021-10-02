142dd054c72d85b762124f711f8f6b7bc937897c
ravialuthge <aluthgeravi@gmail.com>
2021-10-02 20:36:31 +0530

    update

--- plugins/cgroup.py
+++ plugins/cgroup.py
@@ -3,19 +3,28 @@
 
 import os
 from termcolor import colored
+from sdk.docker_info import *
 
-class Cgroup(object):
+class Cgroup(dockerinfo):
     """Confirm default cgroup usage"""
     def __init__(test):
-        test.cgroup_cmd = "ps -ef | grep docker | grep 'cgroup-parent'"
+        test.cgroup_cmd=[] 
         
     def cgroup_scan(test):
-        
-        cgroup_output = os.popen(test.cgroup_cmd).read()
-        cgroup_output_a = cgroup_output.rstrip()
-        cgroup_output = "cgroup=" + cgroup_output_a
+        super().__init__()
+        word = " 'CgroupDriver':"
+        vv = test.lst_dir
+        for h in vv:
+            if word in h:
+              _h = h.split(":")
+              _cgroup_output  = _h[1]
+              bbc = _cgroup_output.replace(" '",'')
+              cgroup_output = bbc.replace("'",'')
+        #cgroup_output = os.popen(test.cgroup_cmd).read()
+        #cgroup_output_a = cgroup_output.rstrip()
+        #cgroup_output = "cgroup=" + cgroup_output_a
 
-        if cgroup_output == "cgroup=":
+        if cgroup_output == "cgroup-parent":
             cgroup_output =  colored('PASS   ', 'green') + "default cgroup used"
         else:
            cgroup_output = colored('WARN   ', 'red') + "confirm default cgroup usage"
