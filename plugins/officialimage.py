a61404675194a9c076b263ed70568b3f2f2c7df4
ravialuthge <aluthgeravi@gmail.com>
2021-10-02 16:29:40 +0530

    update

--- plugins/officialimage.py
+++ plugins/officialimage.py
@@ -12,8 +12,8 @@ class officialimage(imageslist):
        test.a_h=[]
      
     def officialimagescan(test):
-         _images = test.a__images_off_ch
-         try:
+            _images = test.a__images_off_ch
+         #try:
             f = open(test.p, "r")
             mystring  = f.read()
 
@@ -33,10 +33,10 @@ class officialimage(imageslist):
             for im in _images:
                if wo in im:
                   test.a_h.append(im)
-            __h = test.a_h[0]
-            _h = __h.split(":")
-            _install_version  = _h[1]
-            bbc =  _install_version.replace(" ",'')
+            __off_image_name = test.a_h[0]
+            _off_image_name = __off_image_name.split(":")
+            off_image_name  = _off_image_name[1]
+            bbc =  off_image_name.replace(" ",'')
             #install_version = bbc.replace("'",'')
             #print (_install_version)
             
@@ -44,5 +44,5 @@ class officialimage(imageslist):
                print (o +" is Docker Official Image")
             else:
                print (o +" not Docker Official Images")
-         except FileNotFoundError:
-            print ("I did not find the Dockerfile at, "+str(test.p))
+         #except FileNotFoundError:
+         #   print ("I did not find the Dockerfile at, "+str(test.p))
