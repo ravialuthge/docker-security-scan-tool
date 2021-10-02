e896c0502afd0775b3783ecd6b5c299a6e640802
ravialuthge <aluthgeravi@gmail.com>
2021-10-02 16:33:00 +0530

    update

--- plugins/officialimage.py
+++ plugins/officialimage.py
@@ -1,19 +1,15 @@
 ###conf dockerfile#
 
-#import os
-import tmp.imagename
-from sdk.images_list import *
+import os
 
-class officialimage(imageslist):
+class officialimage(object):
     """Check Docker Official Image"""
     def __init__(test):
-       from tmp.filepath import FILEPATH 
+       from tmp.filepath import FILEPATH
        test.p = FILEPATH
-       test.a_h=[]
      
     def officialimagescan(test):
-            _images = test.a__images_off_ch
-         #try:
+         try:
             f = open(test.p, "r")
             mystring  = f.read()
 
@@ -24,25 +20,14 @@ class officialimage(imageslist):
                   img = _s[1]
                   s = img.split(':')
                   o = s[0]
-                  tmp.imagename.IMAGENAME = o
-                  #cmd = "docker search --format '{{.IsOfficial}}' --filter is-official=true " + o
-                  #cmdout = os.popen(cmd).read()
-                  #cmdout_a = cmdout.rstrip()
-           
-            wo = " 'is_official':"
-            for im in _images:
-               if wo in im:
-                  test.a_h.append(im)
-            __off_image_name = test.a_h[0]
-            _off_image_name = __off_image_name.split(":")
-            off_image_name  = _off_image_name[1]
-            bbc =  off_image_name.replace(" ",'')
-            #install_version = bbc.replace("'",'')
-            #print (_install_version)
-            
-            if bbc == 'True':
-               print (o +" is Docker Official Image")
-            else:
-               print (o +" not Docker Official Images")
-         #except FileNotFoundError:
-         #   print ("I did not find the Dockerfile at, "+str(test.p))
+                  
+                  cmd = "docker search --format '{{.IsOfficial}}' --filter is-official=true " + o
+                  cmdout = os.popen(cmd).read()
+                  cmdout_a = cmdout.rstrip()
+          
+                  if cmdout_a == '[OK]':
+                     print (o +" is Docker Official Image")
+                  else:
+                     print (o +" not Docker Official Images")
+         except FileNotFoundError:
+            print ("I did not find the Dockerfile at, "+str(test.p))
