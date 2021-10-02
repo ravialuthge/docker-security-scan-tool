713cba3ae65cee66f51b68033649464a91ee3dc3
ravialuthge <aluthgeravi@gmail.com>
2021-10-02 16:07:53 +0530

    update

--- plugins/officialimage.py
+++ plugins/officialimage.py
@@ -30,21 +30,21 @@ class officialimage(imageslist):
                   #cmd = "docker search --format '{{.IsOfficial}}' --filter is-official=true " + o
                   #cmdout = os.popen(cmd).read()
                   #cmdout_a = cmdout.rstrip()
-                  images = test.__images_off_ch
-                  wo = " 'is_official':"
-                  for im in images:
-                   if wo in im:
-                     test.a_h.append(im)
-                  __h = test.a_h[0]
-                  _h = __h.split(":")
-                  _install_version  = _h[1]
-                  bbc =  _install_version.replace(" ",'')
-                  #install_version = bbc.replace("'",'')
-                  #print (_install_version)
-                  
-                  if bbc == 'True':
-                     print (o +" is Docker Official Image")
-                  else:
-                     print (o +" not Docker Official Images")
+            images = test.__images_off_ch
+            wo = " 'is_official':"
+            for im in images:
+               if wo in im:
+                  test.a_h.append(im)
+            __h = test.a_h[0]
+            _h = __h.split(":")
+            _install_version  = _h[1]
+            bbc =  _install_version.replace(" ",'')
+            #install_version = bbc.replace("'",'')
+            #print (_install_version)
+            
+            if bbc == 'True':
+               print (o +" is Docker Official Image")
+            else:
+               print (o +" not Docker Official Images")
          except FileNotFoundError:
             print ("I did not find the Dockerfile at, "+str(test.p))
