00b81df66d3e51b02b84c0bfc728ed0484342856
ravialuthge <aluthgeravi@gmail.com>
2021-10-03 08:18:54 +0530

    update

--- other_modules/auditctl.py
+++ other_modules/auditctl.py
@@ -1,10 +1,10 @@
 class Audit(object):
     
     def __init__(test):
-        auditcontainerd_cmd = "/usr/bin/docker-containerd" 
-        au = "/etc/audit/audit.rules"
-        _au = au
-        _auditcontainerd_cmd = auditcontainerd_cmd
+        _auditcontainerd_cmd = "/usr/bin/docker-containerd" 
+        _au = "/etc/audit/audit.rules"
+        #_au = au
+        #_auditcontainerd_cmd = auditcontainerd_cmd
         _fi = open(_au, "r")
         _mystring  = _fi.read()
         for it in _mystring.split("\n"):
