275a72eb28bce8fecadce8e56dcf1c047d092d31
ravialuthge <aluthgeravi@gmail.com>
2021-10-03 08:27:30 +0530

    update

--- plugins/auditcontainerd.py
+++ plugins/auditcontainerd.py
@@ -11,7 +11,7 @@ class AuditContainerd(Audit):
 	def __init__(test):
 		super().__init__()
 	def auditcontainerd_scan(test):
-		auditcontainerd_output = test._auditcontainerd_output
+		auditcontainerd_output = str(test._auditcontainerd_output)
 		print (auditcontainerd_output)
 		if auditcontainerd_output == "[]":
 	    		auditcontainerd_re = colored('WARN   ', 'red') + "Add a rule for /usr/bin/docker-containerd file"
