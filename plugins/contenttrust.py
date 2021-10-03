45d37cb2e4db16944aff76f9ca1a5062dd639b77
ravialuthge <aluthgeravi@gmail.com>
2021-10-03 12:29:09 +0530

    update

--- plugins/contenttrust.py
+++ plugins/contenttrust.py
@@ -8,16 +8,12 @@ class contenttrust(object):
 	"""Enable Content trust for Docker"""
 	def __init__(test):
 		test.contenttrust_cmd = "1"
-		#test.contenttrust_version_cmd = os.environ['DOCKER_CONTENT_TRUST']
+		test.contenttrust_version_cmd = os.environ.get('DOCKER_CONTENT_TRUST')
 	def contenttrust_scan(test):
-		try:
-			contenttrust_output = os.environ['DOCKER_CONTENT_TRUST']
+			contenttrust_output = test.contenttrust_version_cmd
 
 			if contenttrust_output == test.contenttrust_cmd:
 				contenttrust_re = colored('PASS   ', 'green') + "Enabled Content trust for Docker"
 			else:
 				contenttrust_re = colored('WARN   ', 'red') + "Enable Content trust for Docker"
-			return contenttrust_re
-		except KeyError:
-			contenttrust_re = colored('WARN   ', 'red') + "Enable Content trust for Docker"
 			return contenttrust_re
