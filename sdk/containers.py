7cb46eff9a30f4de8c738acb3a71b3ce3c340593
ravialuthge <aluthgeravi@gmail.com>
2021-10-07 22:44:30 +0530

    update

--- sdk/containers.py
+++ sdk/containers.py
@@ -1,58 +1,71 @@
 import docker
 
 class ContainerList(object):
-    def __init__(test): 
-        lst=[]
-        con_name_lst=[]
-        con_img_lst=[]
-        _container_appar_list=[]
-        _container_secc_list=[]
-        _container_hel_list=[]
-        client = docker.from_env()
-        for container in client.containers.list():
+    client = docker.from_env()
+    def container_id():
+        con_id_lst=[]
+
+        for container in __class__.client.containers.list():
                     container_ch_cmd_a = container.id
-                    lst.append(container_ch_cmd_a)
+                    con_id_lst.append(container_ch_cmd_a)
+        
+        return con_id_lst
+
+    def container_name():
+        con_name_lst=[]
+    
+        for container in __class__.client.containers.list():
                     container_name_ch_cmd_a = container.name
-                    con_name_lst.append( container_name_ch_cmd_a)
+                    con_name_lst.append(container_name_ch_cmd_a)
+        
+        return con_name_lst
+
+    def container_img_name():
+        con_img_lst=[]
+    
+        for container in __class__.client.containers.list():
                     container_image_list = container.attrs['Config']['Image']
                     con_img_lst.append(container_image_list)
-                    container_appar_list = container.attrs['AppArmorProfile']
-                    _container_appar_list.append("AppArmorProfile="+container_appar_list)
-                    __container_secc_list = container.attrs['HostConfig']['SecurityOpt']
-                    container_secc_list = str(__container_secc_list)
-                    _container_secc_list.append(container_secc_list)
-                    try:
-                     container_hel_list = container.attrs['Config']['Healthcheck']['Test']
-                     _container_hel_list.append(container_hel_list)
-                    except KeyError:
-                     _container_hel_list.append("<nil>")
-        test.con_name_lst = con_name_lst
-        test.lst = lst 
-        test.con_img_lst = con_img_lst
-        test._container_appar_list = _container_appar_list
-        test._container_secc_list = _container_secc_list
-        test._container_hel_list = _container_hel_list
-                    
-    def container_id(test):
-        test.lst = test.lst        
-        return test.lst
     
-    def container_name(test):
-        test.con_name_lst = test.con_name_lst        
-        return test.con_name_lst
+        return con_img_lst
     
-    def container_img_name(test):
-        test.con_img_lst = test.con_img_lst        
-        return test.con_img_lst
+    def container_appar():
+        _container_appar_list=[]
+        for container in __class__.client.containers.list():
+            container_appar_list = container.attrs['AppArmorProfile']
+            _container_appar_list.append("AppArmorProfile="+container_appar_list)
+        apparmor_profile_str_a_s = _container_appar_list 
+        for i in (apparmor_profile_str_a_s):
+                if i == 'AppArmorProfile=':
+                        apparmor_co = 'Verify AppArmor Profile, if applicable'
+                        apparmor_co_st = colored('WARN  ', 'red')
+                        test.lst_apparmor_co.append(apparmor_co)
+                        test.lst_apparmor_co_st.append(apparmor_co_st)
+                else:
+                        apparmor_co = 'AppArmor Profile available'
+                        apparmor_co_st = colored('PASS  ', 'green')
+                        test.lst_apparmor_co.append(apparmor_co)                      
+                        test.lst_apparmor_co_st.append(apparmor_co_st)
+        return _container_appar_list
     
-    def container_appar(test):
-        test._container_appar_list = test._container_appar_list        
-        return test._container_appar_list
+    def container_secc():
+        
+        _container_secc_list=[]
+        for container in __class__.client.containers.list():
+            __container_secc_list = container.attrs['HostConfig']['SecurityOpt']
+            container_secc_list = str(__container_secc_list)
+            _container_secc_list.append(container_secc_list)
+    
+        return _container_secc_list
 
-    def container_secc(test):
-        test._container_secc_list = test._container_secc_list        
-        return test._container_secc_list
+    def container_hel():
+        
+        _container_hel_list=[]
+        for container in __class__.client.containers.list():
+            try:
+                container_hel_list = container.attrs['Config']['Healthcheck']['Test']
+                _container_hel_list.append(container_hel_list)
+            except KeyError:
+                _container_hel_list.append("<nil>")
     
-    def container_hel(test):
-        test._container_hel_list = test._container_hel_list        
-        return test._container_hel_list
+        return _container_hel_list
