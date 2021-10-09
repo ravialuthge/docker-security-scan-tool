f927246fbd84a4464eefa454cff6a1ed16a85ad4
ravialuthge <aluthgeravi@gmail.com>
2021-10-09 10:25:32 +0530

    update

--- plugins/apparmor.py
+++ plugins/apparmor.py
@@ -7,10 +7,10 @@ from other_modules.print import *
 
 class ApparmorPlugin(Print):
     """Verify AppArmor Profile, if applicable"""
-    def __init__(test):
-        dd=[]
-    def apparmor_scan(test):
+    def __init__(test) -> None:
         super().__init__()
+    def apparmor_scan(test):
+        
         abc = Print.container_appar_print()
         return abc
 
