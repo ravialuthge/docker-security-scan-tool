de6ac087026a69fe57547a76a6b8bf972cf4276d
ravialuthge <aluthgeravi@gmail.com>
2021-10-09 08:36:01 +0530

    update

--- plugins/apparmor.py
+++ plugins/apparmor.py
@@ -7,6 +7,8 @@ from sdk.containers import *
 
 class ApparmorPlugin(ContainerList):
     """Verify AppArmor Profile, if applicable"""
+    def __init__(self) -> None:
+        super().__init__()
     def apparmor_scan():
         
         abc = ContainerList().container_appar()
