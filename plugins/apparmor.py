7aa9843b5225c4dcceddd2bfa8dad54532f66fcf
ravialuthge <aluthgeravi@gmail.com>
2021-10-09 07:10:57 +0530

    update

--- plugins/apparmor.py
+++ plugins/apparmor.py
@@ -1,14 +1,14 @@
 ###Profile containers#
 ###CIS_Version 1.0.0:1.6#
 
-from termcolor import colored
-from tabulate import tabulate
+#from termcolor import colored
+#from tabulate import tabulate
 from other_modules.print import *
 
 class ApparmorPlugin():
     """Verify AppArmor Profile, if applicable"""
     def apparmor_scan():
         
-        Print().container_appar_print()
+        print.Print().container_appar_print()
         
 
