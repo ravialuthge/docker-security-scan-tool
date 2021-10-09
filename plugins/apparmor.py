4e5a8f683888d68d0a75860c62873f2d3e885cb9
ravialuthge <aluthgeravi@gmail.com>
2021-10-09 08:52:56 +0530

    update

--- plugins/apparmor.py
+++ plugins/apparmor.py
@@ -9,7 +9,7 @@ class ApparmorPlugin():
     """Verify AppArmor Profile, if applicable"""
     #def __init__(test) -> None:
     #    super().__init__()
-    def apparmor_scan(test):
+    def apparmor_scan():
         
         abc = ContainerList().container_appar()
         return abc
