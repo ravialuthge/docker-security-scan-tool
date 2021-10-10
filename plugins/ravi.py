7996e64b41a4b5bdb1f704b56bcbf379235aacd1
ravialuthge <aluthgeravi@gmail.com>
2021-10-10 10:37:33 +0530

    update

---
+++ plugins/ravi.py
@@ -0,0 +1,15 @@
+###conf dockerfile#
+
+from other_modules.print import Print
+
+from sdk.images import ImagesList
+
+class ravi(Print):
+    """Check Docker Official Image"""
+    
+     
+    def ravi(test):
+       from tmp.filepath import FILEPATH
+       dockerfile_path = FILEPATH
+       officialimag = Print.host_officialimagescan_print(dockerfile_path)
+       return officialimag
